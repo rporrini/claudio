44decb35e7f2fbb14535cb8c93fcd506758b350a
rporrini <r.porrini@gmail.com>
2020-04-11 19:09:53 +0200

    build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const html = require('html-webpack-plugin')
+const copy = require('copy-webpack-plugin');
+
+module.exports = () => {
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      app: ['./application.js'],
+    },
+    output: {
+      filename: '[name].[hash].js',
+      path: __dirname + "/docs",
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+        ]
+    },
+    plugins: []
+      .concat(new html({
+        filename: 'index.html',
+        template: 'index.html',
+        inject: 'head',
+        hash: true
+      }),
+      new copy([
+        { from: 'assets', to: 'assets' }
+      ]))
+  }
+}
