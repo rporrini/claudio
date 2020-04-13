d4f14b917607dbf32d2bd438e16e9dee0ca58e95
rporrini <r.porrini@gmail.com>
2020-04-13 18:57:05 +0200

    refactor

--- test/minutes.test.js
+++ test/minutes.test.js
@@ -1,9 +1,11 @@
 import { readable } from '../src/minutes'
 
-test('should print the minutes in a humar readable form', () => {
-    expect(readable(2)).toBe('2 minuti fa')
-})
+describe('readable', () => {
+    test('should print the minutes in a humar readable form', () => {
+        expect(readable(2)).toBe('2 minuti fa')
+    })
 
-test('should print the seconds under a certain threshold', () => {
-    expect(readable(1)).toBe('pochi secondi fa')
+    test('should print the seconds under a certain threshold', () => {
+        expect(readable(1)).toBe('pochi secondi fa')
+    })
 })
