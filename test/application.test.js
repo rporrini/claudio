5e98905d7274a4fcc943203c16260555eeb17607
rporrini <r.porrini@gmail.com>
2020-04-13 18:22:16 +0200

    no double compilation

--- test/application.test.js
+++ test/application.test.js
@@ -1,15 +1,40 @@
 import { compile as compilation } from "./webpack.testdouble"
 
+let compiledApplication;
+let index;
+
 beforeAll(async () => {
-    const compiledApplication = await compilation()
-    document.documentElement.innerHTML = compiledApplication.readFileSync('docs/index.html', 'utf8')
+    compiledApplication = await compilation()
+    index = compiledApplication.readFileSync('docs/index.html', 'utf8')
+    document.documentElement.innerHTML = index
     require('../src/application')
 })
 
-test('should attach the last updated string to the page', () => {
-    expect(document.body.innerHTML).toMatch('aggiornamento')
+describe('Webpack compilation', () => {
+    
+    test('webpack should compile', () => {
+        expect(compiledApplication).toBeTruthy()
+    })
+    
+    test('webpack should copy the assets', () => {
+        const assets = compiledApplication.readdirSync('docs/assets')
+    
+        expect(assets).toContain('mystery.png')
+        expect(assets).toContain('pregnancy.gif')
+    })
+    
+    test('webpack should include the app in the index', () => {
+        expect(index).toMatch('app')
+    })
 })
 
-test('should attach the last updated minute string to the page', () => {
-    expect(document.body.innerHTML).toMatch(/minuti|secondi/)
+describe('application', () => {
+
+    test('should attach the last updated string to the page', () => {
+        expect(document.body.innerHTML).toMatch('aggiornamento')
+    })
+
+    test('should attach the last updated minute string to the page', () => {
+        expect(document.body.innerHTML).toMatch(/minuti|secondi/)
+    })
 })
